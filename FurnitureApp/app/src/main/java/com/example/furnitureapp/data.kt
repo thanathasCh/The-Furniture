be58bad61c2c77a6e639ef36a153ca9052863a0f
Thanathas Chawengvorakul <tamasama077@gmail.com>
2020-03-28 15:20:44 +0700

    add data class

---
+++ FurnitureApp/app/src/main/java/com/example/furnitureapp/data.kt
@@ -0,0 +1,30 @@
+package com.example.furnitureapp
+
+import com.example.furnitureapp.models.Categories
+
+val categories = arrayListOf(
+    Categories(
+        "Table",
+        R.drawable.desk
+    ),
+    Categories(
+        "Chair",
+        R.drawable.chair
+    ),
+    Categories(
+        "Matress",
+        R.drawable.mattress
+    ),
+    Categories(
+        "Closet",
+        R.drawable.closet
+    ),
+    Categories(
+        "Bed",
+        R.drawable.bed
+    ),
+    Categories(
+        "More",
+        R.drawable.more
+    )
+)
